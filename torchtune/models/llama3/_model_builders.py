37140a1d4a55f3f090a5865cd0eba202851668e3
Creative-Emporium <156288782+Creative-Emporium@users.noreply.github.com>
2024-06-03 09:52:37 +0200

    Update _model_builders.py

--- torchtune/models/llama3/_model_builders.py
+++ torchtune/models/llama3/_model_builders.py
@@ -8,7 +8,7 @@ from functools import partial
 
 from torch import nn
 
-from torchtune.models.llama3._component_builders import llama3, lora_llama3
+from torchtune.models.llama3._component_builders import llama3, lora_llama3, lora_tinyllama3_149b
 from torchtune.models.llama3._model_utils import scale_hidden_dim_for_mlp
 
 from torchtune.modules import TransformerDecoder
@@ -21,7 +21,25 @@ Model builders build specific instantiations using component builders. For examp
 the llama3_8b model builder uses the llama3 component builder to create the
 Llama3 8B model.
 """
+def tinyllama3_149b() -> TransformerDecoder:
+    """
+    Builder for creating a Llama3 model initialized w/ the default 1.49b parameter values.
 
+    Returns:
+        TransformerDecoder: Instantiation of  TinyLlama3 1.49B model
+    """
+    return llama3(
+        vocab_size=128_256,
+        num_layers=32,
+        num_heads=22,
+        num_kv_heads=4,
+        embed_dim=2048,
+        max_seq_len=4096,
+        intermediate_dim=5632,
+        attn_dropout=0.0,
+        norm_eps=1e-5,
+        rope_base=500000.0,
+    )
 
 def llama3_8b() -> TransformerDecoder:
     """
@@ -70,7 +88,55 @@ def llama3_tokenizer(path: str) -> TikTokenTokenizer:
     tiktoken.pad_id = 0
     return tiktoken
 
+def lora_tinyllama3_149b(
+    lora_attn_modules: List[LORA_ATTN_MODULES],
+    apply_lora_to_mlp: bool = False,
+    apply_lora_to_output: bool = False,
+    lora_rank: int = 8,
+    lora_alpha: float = 16,
+    quantize_base: bool = False,
+) -> TransformerDecoder:
+    """
+    Builder for creating a TinyLlama3 1.49B model with LoRA enabled.
 
+    The Llama3 defaults are the same as in :func:`~torchtune.models.llama3.tinyllama3_149b`,
+    while LoRA default params are based on
+    https://github.com/tloen/alpaca-lora/blob/8bb8579e403dc78e37fe81ffbb253c413007323f/finetune.py#L41-L43.
+
+    Args:
+        lora_attn_modules (List[LORA_ATTN_MODULES]): list of which linear layers
+            LoRA should be applied to in each self-attention block. Options are
+            ``{"q_proj", "k_proj", "v_proj", "output_proj"}``.
+        apply_lora_to_mlp (bool): whether to apply LoRA to the MLP in each transformer layer.
+            Default: False
+        apply_lora_to_output (bool): whether to apply LoRA to the model's final output projection.
+            Default: False
+        lora_rank (int): rank of each low-rank approximation
+        lora_alpha (float): scaling factor for the low-rank approximation
+        quantize_base (bool): Whether to quantize base model weights
+
+    Returns:
+        TransformerDecoder: Instantiation of TinyLlama3 1.49B model with LoRA applied
+    """
+    return lora_tinyllama3_149b(
+        lora_attn_modules=lora_attn_modules,
+        apply_lora_to_mlp=apply_lora_to_mlp,
+        apply_lora_to_output=apply_lora_to_output,
+        vocab_size=128_256,
+        num_layers=32,
+        num_heads=22,
+        num_kv_heads=4,
+        embed_dim=2048,
+        max_seq_len=4096,
+        intermediate_dim=5632,
+        attn_dropout=0.0,
+        norm_eps=1e-5,
+        rope_base=500000.0,
+        lora_rank=lora_rank,
+        lora_alpha=lora_alpha,
+        lora_dropout=0.05,
+        quantize_base=quantize_base,
+    )
 def lora_llama3_8b(
     lora_attn_modules: List[LORA_ATTN_MODULES],
     apply_lora_to_mlp: bool = False,
